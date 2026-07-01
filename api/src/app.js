96c21db9c3c9dd7cae7dccb6e5610083de9fbdbc
George Zhu <georgezhu211@gmail.com>
2026-06-30 22:59:06 -0500

    feat: enable cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use(cors());
+
+app.get("/", (req, res) => res.json({ message: "Access granted!" }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
