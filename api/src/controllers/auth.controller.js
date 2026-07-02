15755e305403b30249e3ced687b710ead65af6a3
George Zhu <georgezhu211@gmail.com>
2026-07-01 22:03:23 -0500

    feat: add password hashing with bcryptjs

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,12 +1,15 @@
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const userRepository = require("../repositories/user.repository");
 
 exports.signup = async (req, res) => {
   const { username, password } = req.body;
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await userRepository.create({
     username,
-    password,
+    password: hashedPassword,
     role: "ADMIN",
   });
 
@@ -22,7 +25,7 @@ exports.login = async (req, res) => {
     return res.status(400).json({ error: "Incorrect username" });
   }
 
-  const match = user.password === password;
+  const match = await bcrypt.compare(password, user.password);
 
   if (!match) {
     return res.status(400).json({ error: "Incorrect password" });
