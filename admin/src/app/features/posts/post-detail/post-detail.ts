e1397a38644475bbe52c857b28ffdd852019e5a4
George Zhu <georgezhu211@gmail.com>
2026-07-07 23:36:03 -0500

    feat: add posts components

---
+++ admin/src/app/features/posts/post-detail/post-detail.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-detail',
+  imports: [],
+  templateUrl: './post-detail.html',
+})
+export class PostDetail {}
---
+++ admin/src/app/features/posts/post-form/post-form.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form',
+  imports: [],
+  templateUrl: './post-form.html',
+})
+export class PostForm {}
