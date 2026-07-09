5809960a17f17fcc76d76b2257ba3ed7e94439ca
George Zhu <georgezhu211@gmail.com>
2026-07-08 22:59:55 -0500

    feat: implement post list

--- admin/src/app/features/posts/post-list/post-list.ts
+++ admin/src/app/features/posts/post-list/post-list.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PostsApi } from '../posts-api';
 
 @Component({
   selector: 'app-post-list',
   imports: [],
   templateUrl: './post-list.html',
 })
-export class PostList {}
+export class PostList {
+  private postsApi = inject(PostsApi);
+  posts = this.postsApi.getAll();
+}
