e742482bb4dee7a5ed808a2f77a8966f8fe69e91
George Zhu <georgezhu211@gmail.com>
2026-07-09 23:25:40 -0500

    feat: add post creation form

--- admin/src/app/features/posts/post-form/post-form.ts
+++ admin/src/app/features/posts/post-form/post-form.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { Post, PostsApi } from '../posts-api';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-post-form',
-  imports: [],
+  imports: [FormField],
   templateUrl: './post-form.html',
 })
-export class PostForm {}
+export class PostForm {
+  private postsApi = inject(PostsApi);
+  private router = inject(Router);
+
+  postModel = signal<Omit<Post, 'id'>>({
+    title: '',
+    content: '',
+  });
+
+  postForm = form(this.postModel);
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    this.postsApi.create(this.postModel()).subscribe(() => {
+      this.router.navigate(['/posts']);
+    });
+  }
+}
